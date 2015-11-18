38c6eff3c3c74282a2539f23bf3297925a3b3cd5
root <root@sandbox.hortonworks.com>
2015-11-18 12:46:18 +0000

    first commit

---
+++ download.py
@@ -0,0 +1,7 @@
+import requests,zipfile,StringIO
+source =requests.get("https://resources.lendingclub.com/LoanStats3d.csv.zip")
+stringio = StringIO.StringIO(source.content)
+unzipped = zipfile.ZipFile(stringio)
+import pandas as pd
+clean_csv = pd.read_csv(unzipped.open('LoanStats3d.csv'),skiprows=1,skipfooter=2,engine='python')
+stored_csv = clean_csv.to_csv('./data/stored_csv.csv')
